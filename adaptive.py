919113e77ae429b901610e539e4076e9f93ab498
wnourse05 <wrn13@case.edu>
2024-09-20 16:01:20 -0400

    Know oscillation of c

--- adaptive.py
+++ adaptive.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import time
-c = 0.1
+c = 0.5
 k = (1-c)/c
-tauA = 50
-tauU = 5
-print(4/(tauA*tauA+tauU*tauU+2*tauU*tauA+3))
+tauA = 0.1/50
+tauU = 0.1/5
+print((4*tauU*tauA)/(tauA*tauA+tauU*tauU+2*tauU*tauA))
 params = nn.ParameterDict({
-            'tauU': nn.Parameter(torch.tensor(0.1/tauU)),
+            'tauU': nn.Parameter(torch.tensor(tauU)),
             'leakU': nn.Parameter(torch.tensor(1.0)),
             'restU': nn.Parameter(torch.tensor(0.0)),
             'biasU': nn.Parameter(torch.tensor(0.0)),
             'initU': nn.Parameter(torch.tensor(0.0)),
-            'tauA': nn.Parameter(torch.tensor(0.1/tauA)),
+            'tauA': nn.Parameter(torch.tensor(tauA)),
             'leakA': nn.Parameter(torch.tensor(1.0)),
             'restA': nn.Parameter(torch.tensor(0.0)),
             'gainA': nn.Parameter(torch.tensor(k)),
